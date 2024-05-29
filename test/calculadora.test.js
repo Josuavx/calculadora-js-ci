9af315d42031563c232056e2928e00ad19fedf2b
Josue Avelino de Lima <1241302437@acd.grupotiradentes.com>
2024-05-29 20:28:02 -0300

    style: mudando improtação

---
+++ test/calculadora.test.js
@@ -0,0 +1,12 @@
+import { somar } from '../src/calculadora';
+
+test('Somar dois números', () => {
+    expect(somar(2, 3)).toBe(5);
+
+    expect(somar(-1, -1)).toBe(-2);
+
+    expect(somar(2.5, 2.25)).toBe(4.75);
+
+    expect(somar(10, 0)).toBe(10);
+
+});
